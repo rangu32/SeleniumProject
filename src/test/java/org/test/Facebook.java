7e012660363630fb5e05b90db3711c40186118bf
rangu32 <rangu32@gmail.com>
2023-08-03 13:54:05 +0530

    added facebook url, email, password

--- src/test/java/org/test/Facebook.java
+++ src/test/java/org/test/Facebook.java
@@ -1,17 +1,30 @@
 package org.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Facebook {
-WebDriver dr;
     @Test
     public void LaunchFacebook(){
 
         WebDriver dr=new ChromeDriver();
+        dr.get("https://www.facebook.com/");
 
-        dr.get("www.facebook.com");
+
+        String title = dr.getTitle();
+
+        System.out.println(title);
+
+        String currentUrl = dr.getCurrentUrl();
+        System.out.println(currentUrl);
+
+        WebElement email = dr.findElement(By.id("email"));
+        email.sendKeys("rangu32");
+        WebElement pass = dr.findElement(By.id("pass"));
+        pass.sendKeys("ranga");
 
 
     }
